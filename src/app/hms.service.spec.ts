b50d8f240f7e833e0e74ed00b3ce5950f5c90097
chandra <shekar.bk@gmail.com>
2021-02-01 15:14:46 +0100

    service test case added

--- src/app/hms.service.spec.ts
+++ src/app/hms.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
 import { HmsService } from './hms.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('HmsService', () => {
   let service: HmsService;
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(HmsService);
+
   });
 
   it('should be created', () => {
